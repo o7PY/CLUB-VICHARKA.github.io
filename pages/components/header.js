c822c1769f0f77b86356465b546b1111f91f30cc
Pritesh Jogdhanakar <129420569+pritesh299@users.noreply.github.com>
2024-06-03 19:15:13 +0530

    added header

--- pages/components/header.js
+++ pages/components/header.js
@@ -6,19 +6,18 @@ export default function GenerateHeader(){
     headerContainers.forEach(((header)=>{
 
     const  headerHTML=`
-      <div class="headerContainer">
-      <div class="logo">
-
-        </div>
-        <nav>
-            <a href="${origin}/index.html">HOME</a>
-            <a href="${origin}/pages/members.html">MEMEBERS</a>
-            <a href="${origin}/pages/events.html">EVENTS</a>
-            <a href="${origin}/pages/projects.html">PROJECTS</a>
-            <a href="${origin}/pages/contact.html">CONTACT US</a>
-        </nav>
-      </div>    
-
+    <div class="headerContainer bg-white text-[#070F61] border-b border-[#070F61] p-4 flex items-center justify-between">
+    <div class="logo text-2xl font-semibold">
+          CLUB VICHARKA
+    </div>
+    <nav class="flex space-x-4">
+      <a href="${origin}/index.html" class="font-semibold hover:text-black hover:underline">HOME</a>
+      <a href="${origin}/pages/members.html" class="font-semibold hover:text-black hover:underline">MEMBERS</a>
+      <a href="${origin}/pages/events.html" class="font-semibold hover:text-black hover:underline">EVENTS</a>
+      <a href="${origin}/pages/projects.html" class="font-semibold hover:text-black hover:underline">PROJECTS</a>
+      <a href="${origin}/pages/contact.html" class="font-semibold hover:text-black hover:underline">CONTACT US</a>
+    </nav>
+  </div>
       `
 
       header.innerHTML=headerHTML

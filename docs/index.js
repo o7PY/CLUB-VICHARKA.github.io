b6e8c310ecabc4213fdc56fdb365e2e8bad7b597
Pritesh jogdhanakar <129420569+pritesh299@users.noreply.github.com>
2024-08-08 23:39:46 +0530

    created cursor and added sponsors

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,23 @@
-import GenerateHeader from "./pages/components/header.js"
-const origin = window.location.origin;
 const headerContainer=document.getElementById("header")
+import GenerateHeader from "./pages/components/header.js"
+import tick, { antihover, hover } from "./pages/components/cursor.js"
+GenerateHeader(headerContainer).
+then(()=>{
 
-GenerateHeader(headerContainer)
+    document.querySelectorAll('.specialHover').forEach(ele => {
+        console.log(ele)
+        ele.style.transition = 'all 2s ease-in-out';
+        // Removed redundant hover event listeners
+        ele.addEventListener('mouseenter', () => {
+            hover();
+        });
+    
+        ele.addEventListener('mouseleave', () => {
+            antihover();
+        });
+    });
+    
+    tick()})
+
+
 

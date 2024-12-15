b730cd7455148f8e6b9a29022929ddf83f5221ed
Pritesh Jogdhankar <129420569+pritesh299@users.noreply.github.com>
2024-12-16 04:25:26 +0530

    merged new website design

--- docs/index.js
+++ docs/index.js
@@ -1,23 +1,53 @@
 const headerContainer=document.getElementById("header")
+const footerContainer=document.getElementById("footer")
 import GenerateHeader from "./pages/components/header.js"
-import tick, { antihover, hover } from "./pages/components/cursor.js"
-GenerateHeader(headerContainer).
-then(()=>{
+import GenerateFooter from "./pages/components/footer.js"
 
-    document.querySelectorAll('.specialHover').forEach(ele => {
-        console.log(ele)
-        ele.style.transition = 'all 2s ease-in-out';
-        // Removed redundant hover event listeners
-        ele.addEventListener('mouseenter', () => {
-            hover();
-        });
-    
-        ele.addEventListener('mouseleave', () => {
-            antihover();
-        });
-    });
-    
-    tick()})
+const blob =document.getElementById("blob")
+document.body.onpointermove = event =>{
+    const {clientX,clientY} = event
+
+    blob.animate({
+        left:`${clientX}px`,
+        top:`${clientY}px`
+    },{duration:1500,fill:"forwards"})
+}
+
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null
+
+let fontEffectElements=document.querySelectorAll(".fontEffect")
+fontEffectElements.forEach((ele)=>{
+    ele.onmouseover = event => {  
+        let iteration = 0;
+      let initialText=event.target.dataset.value
+        clearInterval(interval);
+        
+        interval = setInterval(() => {
+          event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter, index) => {
+              if(index < iteration) {
+                return initialText[index];
+              }
+            
+              return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+          
+          if(iteration >=initialText){ 
+            clearInterval(interval);
+          }
+          
+          iteration += 1 / 3;
+        }, 30);
+      }
+})
+
+GenerateHeader(headerContainer)
+GenerateFooter(footerContainer)
 
 
 
--- docs/pages/components/header.js
+++ docs/pages/components/header.js
@@ -1,19 +1,59 @@
 
 export default async function GenerateHeader(headerContainer){
-    const origin = window.location.origin;
+    const origin = window.location.origin + "/CLUB-VICHARKA.github.io";
     const  headerHTML=`
-    <div class="headerContainer bg-transparent text-white p-4 flex items-center justify-center sticky top-0 ">
-    <nav class="flex text-xl w-full justify-between p-4 mx-8">
-      <div class='specialHover p-2 font-semibold  hover:underline underline-offset-4'><a href="${origin}/index.html" class=" ">HOME</a></div>
-      <div class='specialHover p-2 font-semibold  hover:underline underline-offset-4'><a href="${origin}/pages/members.html" class="">MEMBERS</a></div>
-      <div class='specialHover p-2 font-semibold  hover:underline underline-offset-4'><a href="${origin}/pages/events.html" class=" ">EVENTS</a></div>
-      <div class='specialHover p-2 font-semibold  hover:underline underline-offset-4'><a href="${origin}/pages/projects.html" class="">PROJECTS</a></div>
-      <div class='specialHover p-2 font-semibold  hover:underline underline-offset-4'><a href="${origin}/pages/contact.html" class="">CONTACT US</a></div>
-    </nav>
-  </div>
+    <div class="navbar bg-transparent text-white p-4 top-0 z-10 ">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+          <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            <li><a href="${origin}/index.html">HOME</a></li>
+            <li><a href="${origin}/pages/club.html">CLUB</a></li>
+            <li><a href="${origin}/pages/events.html">EVENTS</a></li>
+            <li><a href="${origin}/pages/projects.html">PROJECTS</a></li>
+            <li><a href="${origin}/pages/rover.html"> ROVER</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="navbar-center hidden w-3/4 lg:flex">
+        <ul class="menu menu-horizontal p-4 mx-8 text-xl w-full justify-around">
+          <li class="specialHover btn btn-ghost p-2 text-xl font-semibold hover:underline underline-offset-4">
+            <a href="${origin}/index.html" class="">HOME</a>
+          </li>
+          <li class="specialHover btn btn-ghost p-2 text-xl font-semibold hover:underline underline-offset-4">
+            <a href="${origin}/pages/club.html" class="">CLUB</a>
+          </li>
+          <li class="specialHover btn btn-ghost p-2 text-xl font-semibold hover:underline underline-offset-4">
+            <a href="${origin}/pages/events.html" class="">EVENTS</a>
+          </li>
+          <li class="specialHover btn btn-ghost p-2 text-xl font-semibold hover:underline underline-offset-4">
+            <a href="${origin}/pages/projects.html" class="">PROJECTS</a>
+          </li>
+          <li class="specialHover btn btn-ghost p-2 text-xl font-semibold hover:underline underline-offset-4">
+            <a href="${origin}/pages/rover.html" class="">ROVER</a>
+          </li>
+        </ul>
+      </div>
+      <div class="navbar-end"></div>
+    </div>
       `
       headerContainer.innerHTML=headerHTML
-      console.log('hello')
+      console.log(origin)
       return true
 }
 
